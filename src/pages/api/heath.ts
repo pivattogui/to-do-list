53ba4b9fd54ba43b909c3d5dec23c114707c1915
pivatto <pivattogui@gmail.com>
2023-05-23 15:12:06 -0300

    vercel cron

--- src/pages/api/heath.ts
+++ src/pages/api/heath.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+//Endpoint para não deixar o planetscale dormir, pois ele é gratuito e dorme depois de x dias sem uso
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
@@ -9,6 +10,12 @@ export default async function handler(
     switch (method) {
         case 'GET': {
             try {
+                await prisma.user.findUnique({
+                    where: {
+                        email: "teste@teste.com"
+                    }
+                })
+
                 return res.status(200).json({ message: 'ok' })
             } catch (err) {
                 return res.status(400).json({ error: err })
